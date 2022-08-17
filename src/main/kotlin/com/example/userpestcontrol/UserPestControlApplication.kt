17e2b937b6a1433662fb1190931043cfaedab640
syahridomualim <syahridomualim@gmail.com>
2022-08-17 20:09:36 +0700

    init project

---
+++ src/main/kotlin/com/example/userpestcontrol/UserPestControlApplication.kt
@@ -0,0 +1,11 @@
+package com.example.userpestcontrol
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UserPestControlApplication
+
+fun main(args: Array<String>) {
+    runApplication<UserPestControlApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/userpestcontrol/UserPestControlApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.userpestcontrol
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserPestControlApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
